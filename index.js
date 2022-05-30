2334aef3636fe9bffd5f5efd3ae892f75c2b9e16
yelyzart <yelyzart@gmail.com>
2022-05-30 19:42:06 +0300

    add unit conversion, wind speed, weather description, and weather icon

--- index.js
+++ index.js
@@ -16,14 +16,26 @@ let current = document.querySelector("h3");
 current.innerHTML = `${day} ${hour}:${minutes}`;
 
 function getInformation(response) {
-  let temp = document.querySelector("span");
-  temp.innerHTML = Math.round(response.data.main.temp);
-  let city = document.querySelector("h2");
-  city.innerHTML = response.data.name;
-  let humidity = document.querySelector("span.hum");
-  humidity.innerHTML = response.data.main.humidity;
+  console.log(response.data);
+  let temp = document.querySelector("span.temperature");
+  let city = document.querySelector("#city");
+  let country = document.querySelector("span.country");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("span.humidity");
   let windspeed = document.querySelector("span.wind");
+  let weatherIcon = document.querySelector("#weather-icon");
+
+  celsiusTemp = response.data.main.temp;
+  temp.innerHTML = Math.round(response.data.main.temp);
+  city.innerHTML = response.data.name;
+  country.innerHTML = response.data.sys.country;
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
   windspeed.innerHTML = response.data.wind.speed;
+  weatherIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 function searchCity(city) {
@@ -35,9 +47,34 @@ function searchCity(city) {
 function enterC(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#enter-city");
-  let city = document.querySelector("h2");
+  let city = document.querySelector("#city");
   city.innerHTML = `${searchInput.value}`;
   searchCity(searchInput.value);
 }
+
+function displayfahrenheitElem(event) {
+  event.preventDefault();
+  celsius.classList.remove("celsius");
+  fahrenheit.classList.add("celsius");
+  let temp = document.querySelector("span.temperature");
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  temp.innerHTML = Math.round(fahrenheitTemp);
+}
+function displaycelsiusElem(event) {
+  event.preventDefault();
+  fahrenheit.classList.remove("celsius");
+  celsius.classList.add("celsius");
+  let temp = document.querySelector("span.temperature");
+  temp.innerHTML = Math.round(celsiusTemp);
+}
+
+let celsiusTemp = null;
+
 let inputForm = document.querySelector("form");
 inputForm.addEventListener("submit", enterC);
+
+let fahrenheit = document.querySelector("a.fahrenheit");
+fahrenheit.addEventListener("click", displayfahrenheitElem);
+
+let celsius = document.querySelector("a.celsius");
+celsius.addEventListener("click", displaycelsiusElem);
