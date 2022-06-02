3552d0b47b02dea7045aaf11a32b3c53cb58f7b9
yelyzart <yelyzart@gmail.com>
2022-06-02 14:26:51 +0300

    forecast moment

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let days = [
 ];
 
 if (hour < 10) {
-  hours = `0${hours}`;
+  hour = `0${hour}`;
 }
 if (minutes < 10) {
   minutes = `0${minutes}`;
@@ -21,6 +21,19 @@ if (minutes < 10) {
 let day = days[now.getDay()];
 let current = document.querySelector("h3");
 current.innerHTML = `${day} ${hour}:${minutes}`;
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  return days[day];
+}
+
+function getForecast(coordinates) {
+  let apiKey = "7462c8bafebf7d5e231ed68152a3e97e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function getInformation(response) {
   console.log(response.data);
@@ -43,9 +56,33 @@ function getInformation(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  getForecast(response.data.coord);
 }
 
 let celsiusTemp = null;
+
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#weather-table");
+  let forecastHTML = "";
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<tr>
+        <td>${formatDay(forecastDay.dt)}</td>
+        <td><img src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" alt="" width="50px" class="forecast-icon"/></td>
+        <td><span>${Math.round(
+          forecastDay.temp.max
+        )}°</span> / <span>${Math.round(forecastDay.temp.min)}°</span></td>
+     </tr>`;
+      forecastElement.innerHTML = forecastHTML;
+    }
+  });
+}
 
 function searchCity(city) {
   let key = "7462c8bafebf7d5e231ed68152a3e97e";
